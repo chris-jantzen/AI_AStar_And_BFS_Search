8eef64e6d3734c1690e81376a0f33d55ea70d39d
Chris Jantzen <jantzeca@mail.uc.edu>
2019-09-19 12:20:01 -0400

    Relocated lists and other structure changes

--- bfs.py
+++ bfs.py
@@ -3,32 +3,28 @@ from priority_queue import PriorityQueue
 from search import Search
 from math import inf
 
-open_list = PriorityQueue()  # List of states that have been seen but not expanded
-closed_list = []  # List of states that have been expanded
-
 
 class BFS(Search):
+    def __init__(self, goal_state):
+        super().__init__(goal_state)
+        self.open_list = PriorityQueue()
+        self.closed_list = []
+
     def breadthFirstSearch(self, start_state):
         found_goal_state = False
         id_count = 1
-        open_list.insert(start_state)
+        self.open_list.insert(start_state)
         while not found_goal_state:
             node = self.getOpenListNode()
-            # TODO: Expand the node here
+            if node.isGoalState(self.goal_state):
+                print('goal state')
+                return
+
+            # TODO:
+            # Expand the node here
             # Then add it to the closed list
             # Implement a way to compare it to things in the closed list,
             # which will basically just be like doing the goal state check.
 
     def getOpenListNode(self):
-        min = inf
-        minState = None
-        for i in open_list:
-            if i.fn < min:
-                min = i.fn
-                minState = i
-        return minState
-
-    def checkStartStateEqualsGoal(self, start):
-        if start.isGoalState(self.goal_state):
-            return True
-        return False
+        return self.open_list.pop()
