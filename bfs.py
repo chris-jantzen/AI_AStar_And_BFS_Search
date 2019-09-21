9161798b07db0890fbef1809ec01b2fd9d661902
Chris Jantzen <jantzeca@mail.uc.edu>
2019-09-20 22:07:57 -0400

    Relocate generic code to base class

--- bfs.py
+++ bfs.py
@@ -1,16 +1,8 @@
-from state import State
-from priority_queue import PriorityQueue
 from search import Search
 
 
 class BFS(Search):
-    def __init__(self, goal_state):
-        super().__init__(goal_state)
-        self.open_list = PriorityQueue()
-        self.closed_list = []
-
     def breadthFirstSearch(self, start_state):
-        self.id_count = 1
         self.open_list.insert(start_state)
         while True:
             node = self.getOpenListNode()
@@ -23,77 +15,3 @@ class BFS(Search):
             # Expands node and inserts all non-duplicates into open list
             self.expandNode(node)
             self.closed_list.append(node)
-
-    def getOpenListNode(self):
-        return self.open_list.pop()
-
-    def expandNode(self, node):
-        zero_index = node.state.index(0)
-        # Indexes where a zero would be on the edge of a board and would have no tile
-        # to the left or right to move into its spot
-        left_no_moves = [4, 8, 12, 16]
-        right_no_moves = [3, 7, 11, 15]
-        newNodes = []
-        # TODO: Abstract this into another function
-        if zero_index - 4 >= 0:
-            newNodes.append(
-                State(
-                    self.swapPositions(node.state.copy(),
-                                       zero_index, zero_index-4),
-                    self.id_count,
-                    node.stateid,
-                    node.gn + 1)
-            )
-            self.id_count += 1
-        if zero_index not in left_no_moves and zero_index - 1 >= 0:
-            newNodes.append(
-                State(
-                    self.swapPositions(node.state.copy(),
-                                       zero_index, zero_index-1),
-                    self.id_count,
-                    node.stateid,
-                    node.gn + 1
-                )
-            )
-            self.id_count += 1
-        if zero_index not in right_no_moves and zero_index + 1 < len(node.state):
-            newNodes.append(
-                State(
-                    self.swapPositions(node.state.copy(),
-                                       zero_index, zero_index+1),
-                    self.id_count,
-                    node.stateid,
-                    node.gn + 1
-                )
-            )
-            self.id_count += 1
-        if zero_index + 4 < len(node.state):
-            newNodes.append(
-                State(
-                    self.swapPositions(node.state.copy(),
-                                       zero_index, zero_index+4),
-                    self.id_count,
-                    node.stateid,
-                    node.gn + 1
-                )
-            )
-            self.id_count += 1
-
-        newNodes = self.checkForDuplicates(newNodes)
-        for i in newNodes:
-            self.open_list.insert(i)
-
-    def checkForDuplicates(self, nodes):
-        to_pop = []
-        for i in nodes:
-            for j in self.open_list:
-                if i.state == j.state:
-                    to_pop.append(i)
-                    break
-            for k in self.closed_list:
-                if i.state == k.state:
-                    to_pop.append(i)
-                    break
-        for item in to_pop:
-            nodes.remove(item)
-        return nodes
--- search.py
+++ search.py
@@ -1,6 +1,13 @@
+from priority_queue import PriorityQueue
+from state import State
+
+
 class Search:
     def __init__(self, goal_state):
         self.goal_state = goal_state
+        self.open_list = PriorityQueue()
+        self.closed_list = []
+        self.id_count = 1
 
     def swapPositions(self, lst, pos1, pos2):
         lst[pos1], lst[pos2] = lst[pos2], lst[pos1]
@@ -20,3 +27,77 @@ class Search:
                     break
         path.reverse()
         return path
+
+    def getOpenListNode(self):
+        return self.open_list.pop()
+
+    def checkForDuplicates(self, nodes):
+        to_pop = []
+        for i in nodes:
+            for j in self.open_list:
+                if i.state == j.state:
+                    to_pop.append(i)
+                    break
+            for k in self.closed_list:
+                if i.state == k.state:
+                    to_pop.append(i)
+                    break
+        for item in to_pop:
+            nodes.remove(item)
+        return nodes
+
+    def expandNode(self, node):
+        zero_index = node.state.index(0)
+        # Indexes where a zero would be on the edge of a board and would have no tile
+        # to the left or right to move into its spot
+        left_no_moves = [4, 8, 12, 16]
+        right_no_moves = [3, 7, 11, 15]
+        newNodes = []
+        # TODO: Abstract this into another function
+        if zero_index - 4 >= 0:
+            newNodes.append(
+                State(
+                    self.swapPositions(node.state.copy(),
+                                       zero_index, zero_index-4),
+                    self.id_count,
+                    node.stateid,
+                    node.gn + 1)
+            )
+            self.id_count += 1
+        if zero_index not in left_no_moves and zero_index - 1 >= 0:
+            newNodes.append(
+                State(
+                    self.swapPositions(node.state.copy(),
+                                       zero_index, zero_index-1),
+                    self.id_count,
+                    node.stateid,
+                    node.gn + 1
+                )
+            )
+            self.id_count += 1
+        if zero_index not in right_no_moves and zero_index + 1 < len(node.state):
+            newNodes.append(
+                State(
+                    self.swapPositions(node.state.copy(),
+                                       zero_index, zero_index+1),
+                    self.id_count,
+                    node.stateid,
+                    node.gn + 1
+                )
+            )
+            self.id_count += 1
+        if zero_index + 4 < len(node.state):
+            newNodes.append(
+                State(
+                    self.swapPositions(node.state.copy(),
+                                       zero_index, zero_index+4),
+                    self.id_count,
+                    node.stateid,
+                    node.gn + 1
+                )
+            )
+            self.id_count += 1
+
+        newNodes = self.checkForDuplicates(newNodes)
+        for i in newNodes:
+            self.open_list.insert(i)
