d4828530bd69005223d974827bc94987214ae3bc
Chris Jantzen <jantzeca@mail.uc.edu>
2019-09-21 10:09:56 -0400

    BFS complete

--- bfs.py
+++ bfs.py
@@ -5,14 +5,16 @@ class BFS(Search):
     def breadthFirstSearch(self, start_state):
         self.open_list.insert(start_state)
         while True:
-            if self._open_list_pop_count < 10000000:
+            if self._open_list_pop_count < 1000000:
                 node = self.getOpenListNode()
             else:
                 print('Impossible to find goal state')
+                return
             if node.isGoalState(self.goal_state):
                 path = self.getPathTaken(node)
-                for i in path:
-                    print(i)
+                # for i in path:
+                #     print(i)
+                print('Length Of Path To Get To Goal: {}'.format(len(path)))
                 print('Nodes Pushed To Open List: {}'.format(
                     self._open_list_add_count))
                 print('Nodes Pushed To Closed List: {}'.format(
